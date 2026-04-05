0cd3d8479a3480a14b2ca930aba3b8477f93213b
sunday2000 <kodonoumahuwanu@gmail.com>
2026-04-05 07:29:19 +0100

    show test code on otp verification form

--- exam-plateform-tp-front/src/api/axios.ts
+++ exam-plateform-tp-front/src/api/axios.ts
@@ -3,8 +3,8 @@ import { getStoredSession } from '@/utils/authStorage'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  //baseURL: 'http://localhost:8000/api',
+  //baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'http://localhost:8000/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
